007cbbf16ad351001e4d719c948f4fc0fcf06eb7
AbdullajonSoliyevFergana <abdullajonesaboyev@gmail.com>
2023-07-07 21:46:36 +0500

    ...

---
+++ data.js
@@ -0,0 +1,41 @@
+
+const products = [
+    {
+        id: 1,
+        name: "Product 1",
+        image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT_Ie7dVUafnrdsNt6F-n8nWiktJb15z7HgvA&usqp=CAU",
+        price: 10000,
+        desc: "Description 1"
+    },
+    {
+        id: 2,
+        name: "Product 2",
+        image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQzWagBmJe5Fi3wC2gV0526jWRPClmVOZXwMg&usqp=CAU",
+        price: 20000,
+        desc: "Description 2"
+    },
+    {
+        id: 3,
+        name: "1Product 3",
+        image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSjKPuLozRC2n4MnlyKlPdTQm6keMAOJnlTwQ&usqp=CAU",
+        price: 30000,
+        desc: "Description 3"
+    },
+]
+
+const peoples = [
+    {
+        id: 1,
+        name: "ali 1"
+    },
+    {
+        id: 2,
+        name: "ali 2"
+    },
+    {
+        id: 3,
+        name: "ali 3"
+    },
+]
+
+module.exports = { products, peoples }
